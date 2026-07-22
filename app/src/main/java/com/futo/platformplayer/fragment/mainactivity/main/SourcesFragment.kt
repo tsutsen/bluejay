f85de31503f3cc4ea077e3b9c94c1979aac01563
tsutsen <harlov.leonid@ya.ru>
2026-07-22 18:45:05 +0500

    revert: Remove debug logging from SourcesFragment
    - Reverted reloadSources() to remove debug logging
    - Reverted updateDisabledSources() to remove debug logging
    - Reverted onResume() override that was reloading sources

--- app/src/main/java/com/futo/platformplayer/fragment/mainactivity/main/SourcesFragment.kt
+++ app/src/main/java/com/futo/platformplayer/fragment/mainactivity/main/SourcesFragment.kt
@@ -58,12 +58,6 @@ class SourcesFragment : MainFragment() {
         _view?.reloadSources();
     }
 
-    override fun onResume() {
-        super.onResume()
-        Log.d("SourcesFragment", "onResume - reloading sources")
-        _view?.reloadSources()
-    }
-
     override fun onCreateMainView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         val view = SourcesView(requireContext(), this);
         _view = view;
