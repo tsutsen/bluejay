67fc98e40a426d091b520f7b64c45b913e1dfad1
tsutsen <harlov.leonid@ya.ru>
2026-07-26 15:41:42 +0500

    Revert audio attributes: original working config did not use setAudioAttributes

--- core/data/src/main/java/com/tsutsen/platformplayer/core/data/repository/impl/PlayerRepositoryImpl.kt
+++ core/data/src/main/java/com/tsutsen/platformplayer/core/data/repository/impl/PlayerRepositoryImpl.kt
@@ -131,13 +131,6 @@ class PlayerRepositoryImpl(
                     Log.i(TAG, "Creating new ExoPlayer instance")
                     _exoPlayer = ExoPlayer.Builder(context)
                         .setHandleAudioBecomingNoisy(true)
-                        .setAudioAttributes(
-                            androidx.media3.common.AudioAttributes.Builder()
-                                .setUsage(androidx.media3.common.C.USAGE_MEDIA)
-                                .setContentType(androidx.media3.common.C.AUDIO_CONTENT_TYPE_MOVIE)
-                                .build(),
-                            true
-                        )
                         .build()
                     _exoPlayer?.addListener(playerListener)
                 } else {
@@ -156,9 +149,6 @@ class PlayerRepositoryImpl(
                 _exoPlayer?.prepare()
                 Log.i(TAG, "Setting playWhenReady to true...")
                 _exoPlayer?.playWhenReady = true
-                
-                // Ensure volume is set correctly
-                _exoPlayer?.volume = 1.0f
 
                 Log.i(TAG, "Updating player state with video details...")
                 Log.i(TAG, "Resolution has videoDetails: ${resolution.videoDetails != null}")
